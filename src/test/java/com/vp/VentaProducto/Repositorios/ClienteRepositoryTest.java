d5c2e7b77dafed300aaf88b97a2c54cca44df80d
nioclandramer <161773056+nioclandramer@users.noreply.github.com>
2024-03-28 18:18:56 -0500

    Repositorys y Correccione

--- src/test/java/com/vp/VentaProducto/Repositorios/ClienteRepositoryTest.java
+++ src/test/java/com/vp/VentaProducto/Repositorios/ClienteRepositoryTest.java
@@ -7,11 +7,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.InstanceOfAssertFactories.OPTIONAL;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 class ClienteRepositoryTest extends AstractIntegrationBDTest {
     ClienteRepository clienteRepository;
@@ -51,7 +51,7 @@ class ClienteRepositoryTest extends AstractIntegrationBDTest {
         cliente.setPedidos(null);
         Cliente clienteSave=clienteRepository.save(cliente);
         //when
-        Optional<Cliente> optionalCliente= clienteRepository.findById(cliente.getId());
+        Optional<Cliente> optionalCliente= clienteRepository.findById(clienteSave.getId());
         //then
         assertThat(optionalCliente).isPresent();
     }
@@ -87,4 +87,60 @@ class ClienteRepositoryTest extends AstractIntegrationBDTest {
         assertThat(clienteRepository.findById(clienteSave.getId())).isEmpty();
     }
 
+    @Test
+    void GiveCliente_whenFindByEmail_thenReturnTheClienteIsFound(){
+        //give
+        Cliente cliente=new Cliente();
+        cliente.setNombre("juan cuna");
+        cliente.setEmail("juacho@gmail.com");
+        cliente.setDireccion("Urbanizacion mayorca manzana #7 casa #200");
+        cliente.setPedidos(null);
+        clienteRepository.save(cliente);
+        //when
+        Optional<Cliente> clienteFound=clienteRepository.findByEmail("juacho@gmail.com");
+        //then
+        assertThat(clienteFound).isPresent();
+    }
+
+    @Test
+    void GiveCliente_whenFindByDireccion_thenReturnListOfClient(){
+        //give
+        Cliente cliente=new Cliente();
+        cliente.setNombre("rober");
+        cliente.setDireccion("casa28");
+        cliente.setEmail("jolsagmail.com");
+        clienteRepository.save(cliente);
+
+        Cliente cliente1=new Cliente();
+        cliente1.setNombre("robertos");
+        cliente1.setDireccion("casa28");
+        cliente1.setEmail("jolsaaagmail.com");
+        clienteRepository.save(cliente1);
+        //when
+        Optional<List<Cliente>> foudnCliente=clienteRepository.findByDireccion("casa28");
+        //then
+        assertThat(foudnCliente).isPresent();
+        assertThat(foudnCliente.get()).hasSize(2);
+    }
+
+    @Test
+    void GiveCliente_whenFindByNombre_thenReturnListOfCliente(){
+        //give
+        Cliente cliente=new Cliente();
+        cliente.setNombre("rober");
+        cliente.setDireccion("casa28");
+        cliente.setEmail("jolsagmail.com");
+        clienteRepository.save(cliente);
+
+        Cliente cliente1=new Cliente();
+        cliente1.setNombre("robertos");
+        cliente1.setDireccion("casa28");
+        cliente1.setEmail("jolsaaagmail.com");
+        clienteRepository.save(cliente1);
+        //when
+        Optional<List<Cliente>> foudnCliente=clienteRepository.findByNombreStartingWith("rober");
+        //then
+        assertThat(foudnCliente).isPresent();
+        assertThat(foudnCliente.get()).hasSize(2);
+    }
 }
