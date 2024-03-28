02aff904ffcaf85d27fe838db256fe56e767be7c
blackbox002 <mauricioecheverri007@gmail.com>
2024-03-28 13:01:08 -0500

    nuevos test query de los repositorios

--- src/test/java/com/vp/VentaProducto/Repositorios/ProductoRepositoryTest.java
+++ src/test/java/com/vp/VentaProducto/Repositorios/ProductoRepositoryTest.java
@@ -100,7 +100,53 @@ class ProductoRepositoryTest extends AstractIntegrationBDTest {
         assertThat(productosFound.size()).isEqualTo(1);
         assertThat(productosFound).isNotEmpty();
 
+    }
+    @Test
+    void findByProductsInStock_WhenFoundInStock_ThenResturnsList(){
+        //Given
+        Producto producto1= new Producto();
+        producto1.setNombre("Crema de Marihuana");
+        producto1.setStock(15);
+        producto1.setPrecio(12);
 
+        Producto producto2= new Producto();
+        producto2.setNombre("Postre de 4 leches");
+        producto2.setStock(12);
+        producto2.setPrecio(5);
+
+        productoRepository.save(producto1);
+        productoRepository.save(producto2);
+        //When
+        Optional<List<Producto>> foundProducts= productoRepository.findByProductStockEqual(1);
+        List<Producto> listPorductsFound =  foundProducts.get();
+        //then
+        assertThat(foundProducts).isPresent();
+        assertThat(listPorductsFound).hasSize(2);
+
+
+    }
+    @Test
+    void findProductsInDeterminatedStockAndPrice_WhenMatch_ThenReturnsAList(){
+        //Given
+        Producto producto1= new Producto();
+        producto1.setNombre("Crema de Marihuana");
+        producto1.setStock(15);
+        producto1.setPrecio(12);
+
+        Producto producto2= new Producto();
+        producto2.setNombre("Postre de 4 leches");
+        producto2.setStock(12);
+        producto2.setPrecio(5);
+
+        productoRepository.save(producto1);
+        productoRepository.save(producto2);
+
+        //When
+        Optional<List<Producto>> foundProducts= productoRepository.findByPrecioAndStock(5,16);
+        //then
+        assertThat(foundProducts).isPresent();
+        List<Producto> productoList= foundProducts.get();
+        assertThat(productoList).hasSize(1);
 
     }
 }
