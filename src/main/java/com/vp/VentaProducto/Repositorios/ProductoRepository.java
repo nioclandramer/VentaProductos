2fb01f67310ad99a23ec3932fdfb34ef447e7e4e
Leonard Ferreira <leonardferreiraeb@unimagdalena.edu.co>
2024-03-28 11:27:11 -0500

    Repositorios

--- src/main/java/com/vp/VentaProducto/Repositorios/ProductoRepository.java
+++ src/main/java/com/vp/VentaProducto/Repositorios/ProductoRepository.java
@@ -2,11 +2,15 @@ package com.vp.VentaProducto.Repositorios;
 
 import com.vp.VentaProducto.Entidades.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductoRepository extends JpaRepository <Producto,Long>{
-    //List<Producto> findByNombreContainingIgnoreCase(String termino);
-    //List<Producto> findByStockGreaterThanEqual(Integer num);
-    //List<Producto> findByPrecioLessThanEqualAndStockLessThanEqual(Integer precio,Integer stock);
+    List<Producto> findByNombreContainingIgnoreCase(String termino);
+    @Query("SELECT p FROM Producto p WHERE p.Stock >= :num")
+    Optional<List<Producto>> findByStockGreaterThanEqual(Integer num);
+    @Query("SELECT p FROM Producto p WHERE p.precio <= :precio AND p.Stock <= :stock")
+    Optional<List<Producto>> findByPrecioLessThanEqualAndStockLessThanEqual(Integer precio, Integer stock);
 }
