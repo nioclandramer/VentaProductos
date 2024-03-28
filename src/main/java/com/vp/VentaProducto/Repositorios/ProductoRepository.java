1a6188340e05926e0e106cd50b107b45b5125a2b
blackbox002 <mauricioecheverri007@gmail.com>
2024-03-28 12:41:09 -0500

    xd

--- src/main/java/com/vp/VentaProducto/Repositorios/ProductoRepository.java
+++ src/main/java/com/vp/VentaProducto/Repositorios/ProductoRepository.java
@@ -10,8 +10,8 @@ import java.util.Optional;
 public interface ProductoRepository extends JpaRepository <Producto,Long>{
     List<Producto> findByNombreContainingIgnoreCase(String termino);
     @Query("SELECT p FROM Producto p WHERE p.Stock >= :num")
-    Optional<List<Producto>> findByStockGreaterThanEqual(Integer num);
+    Optional<List<Producto>> findByProductStockEqual(Integer num);
     @Query("SELECT p FROM Producto p WHERE p.precio <= :precio AND p.Stock <= :stock")
-    Optional<List<Producto>> findByPrecioLessThanEqualAndStockLessThanEqual(Integer precio, Integer stock);
+    Optional<List<Producto>> findByPrecioAndStock(Integer precio, Integer stock);
     //sdasdadsd
 }
