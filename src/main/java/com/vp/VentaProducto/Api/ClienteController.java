b24ebbaeb4c9c589c242eca544375c04871c9eb8
nioclandramer <161773056+nioclandramer@users.noreply.github.com>
2024-03-31 13:59:11 -0500

    solve

--- src/main/java/com/vp/VentaProducto/Api/ClienteController.java
+++ src/main/java/com/vp/VentaProducto/Api/ClienteController.java
@@ -1,1 +1,28 @@
+package com.vp.VentaProducto.Api;
 
+import com.vp.VentaProducto.Dtos.Cliente.ClienteDto;
+import com.vp.VentaProducto.Servicios.ClienteService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("Api/v1/clientes")
+public class ClienteController {
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<Optional<List<ClienteDto>>> getClientes(){
+        Optional<List<ClienteDto>> clienteDtos=clienteService.getAllClientes();
+        return ResponseEntity.ok().body(clienteDtos);
+    }
+
+}
