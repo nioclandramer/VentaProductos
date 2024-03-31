506a5be5db797804f1b6c178b18d5479e268f142
blackbox002 <mauricioecheverri007@gmail.com>
2024-03-31 16:17:14 -0500

    Controlladores

--- src/main/java/com/vp/VentaProducto/Dtos/Cliente/ClienteMapper.java
+++ src/main/java/com/vp/VentaProducto/Dtos/Cliente/ClienteMapper.java
@@ -1,8 +1,9 @@
 package com.vp.VentaProducto.Dtos.Cliente;
 
 import com.vp.VentaProducto.Entidades.Cliente;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
-
+@Mapper
 public interface ClienteMapper {
     ClienteMapper INSTANCE = Mappers.getMapper(ClienteMapper.class);
     ClienteDto clienteToDto(Cliente cliente);
