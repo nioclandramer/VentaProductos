31c7504a8f1ab0b5ec2c39032b30d638d642fd69
nioclandramer <161773056+nioclandramer@users.noreply.github.com>
2024-03-31 15:33:40 -0500

    Controllers

--- src/main/java/com/vp/VentaProducto/Api/ClienteController.java
+++ src/main/java/com/vp/VentaProducto/Api/ClienteController.java
@@ -1,11 +1,11 @@
 package com.vp.VentaProducto.Api;
 
 import com.vp.VentaProducto.Dtos.Cliente.ClienteDto;
+import com.vp.VentaProducto.Dtos.Cliente.ClienteToSaveDto;
 import com.vp.VentaProducto.Servicios.ClienteService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +17,47 @@ public class ClienteController {
 
     public ClienteController(ClienteService clienteService) {
         this.clienteService = clienteService;
+    }
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody ClienteToSaveDto cliente){
+        clienteService.guardarCliente(cliente);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<ClienteDto>> getProductoByID(@PathVariable Long id){
+        Optional<ClienteDto> cliente= Optional.ofNullable(clienteService.findById(id));
+        return ResponseEntity.ok(cliente);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> update(@RequestBody ClienteToSaveDto cliente){
+        clienteService.actualizarCliente(cliente);
+        return  ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        clienteService.deleteByID(id);
+        return  ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/searchT")
+    public ResponseEntity<Optional<ClienteDto>>searchClienteEmail(@RequestParam("searchT") String emailT){
+        Optional<ClienteDto> cliente=clienteService.findByEmail(emailT);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @GetMapping("/searchT")
+    public ResponseEntity<Optional<List<ClienteDto>>>searchClientesDireccion(@RequestParam("searchT") String direccionT){
+        Optional<List<ClienteDto>> cliente=clienteService.findByDireccion(direccionT);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @GetMapping("/searchT")
+    public ResponseEntity<Optional<List<ClienteDto>>>searchClientesNombreStartingWith(@RequestParam("searchT") String nombreT){
+        Optional<List<ClienteDto>> cliente=clienteService.findByNombreStartingWith(nombreT);
+        return ResponseEntity.ok(cliente);
     }
 
     @GetMapping()
