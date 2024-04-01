2d628b06fa90d1d927ecf2beaffabf3346a0e999
nioclandramer <161773056+nioclandramer@users.noreply.github.com>
2024-04-01 16:45:08 -0500

    service test

---
+++ src/test/java/com/vp/VentaProducto/Servicios/ClienteServiceIMPLTest.java
@@ -0,0 +1,118 @@
+package com.vp.VentaProducto.Servicios;
+
+import com.vp.VentaProducto.Dtos.Cliente.ClienteDto;
+import com.vp.VentaProducto.Dtos.Cliente.ClienteToSaveDto;
+import com.vp.VentaProducto.Entidades.Cliente;
+import com.vp.VentaProducto.Exception.ClienteNotFoundException;
+import com.vp.VentaProducto.Repositorios.ClienteRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+class ClienteServiceIMPLTest {
+
+    @Mock
+    private ClienteRepository clienteRepository;
+
+    @InjectMocks
+    private ClienteServiceIMPL clienteService;
+    Cliente cliente;
+    @BeforeEach
+    void setUp(){
+        cliente=new Cliente();
+        cliente.setId(1L);
+        cliente.setNombre("test1");
+        cliente.setEmail("test1@gmail");
+        cliente.setDireccion("test1direccion");
+        cliente.setPedidos(null);
+        clienteRepository.save(cliente);
+    }
+
+    @Test
+    void GiveCliente_WhenClienteCreate_ThenReturnClienteGuardado(){
+        cliente.setId(1L);
+        cliente.setNombre("test1");
+        cliente.setEmail("test1@gmail");
+        cliente.setDireccion("test1direccion");
+        cliente.setPedidos(null);
+        given(clienteRepository.save(any())).willReturn(cliente);
+        //Given
+
+        ClienteToSaveDto clienteGuardado=new ClienteToSaveDto(
+                1L,
+                "test1",
+                "test1@gmail",
+                "test1direccion");
+        //When
+        ClienteDto clienteDto=clienteService.guardarCliente(clienteGuardado);
+        //then
+        assertThat(clienteDto).isNotNull();
+        assertThat(clienteDto.id()).isEqualTo(1L);
+    }
+
+    @Test
+    void GiveClienteId_whenFindClienteById_thenReturnCliente(){
+        Long clienteID=1L;
+        //given
+        given(clienteRepository.findById(clienteID)).willReturn(Optional.ofNullable(cliente));
+        //when
+        ClienteDto clienteDto=clienteService.findById(clienteID);
+        //then
+        assertThat(clienteDto).isNotNull();
+    }
+
+    @Test
+    void GiveClienteId_whenFindClienteById_thenReturnException(){
+        //given
+        given(clienteRepository.findById(any())).willReturn(Optional.ofNullable(null));
+        assertThrows(ClienteNotFoundException.class,()->{
+            clienteService.findById(any());
+        },"Cliente No Encontrado");
+    }
+
+    @Test
+    void GivenCliente_WhenClienteUpdate_ThenClienteUpdate(){
+        cliente.setId(1L);
+        cliente.setNombre("test1");
+        cliente.setEmail("test1@gmail");
+        cliente.setDireccion("test1direccion");
+        cliente.setPedidos(null);
+        given(clienteRepository.save(any())).willReturn(cliente);
+        //Given
+
+        ClienteToSaveDto clienteGuardado=new ClienteToSaveDto(
+                1L,
+                "test1",
+                "test1@gmail",
+                "test1direccion");
+        ClienteDto clienteDto=clienteService.guardarCliente(clienteGuardado);
+        //when
+        cliente.setNombre("test2");
+        clienteDto=clienteService.guardarCliente(clienteGuardado);
+        //then
+        assertThat(clienteDto).isNotNull();
+        assertThat(clienteDto.id()).isEqualTo(1L);
+    }
+
+    @Test
+    void GiveClienteId_WhenDeleteByClienteId_thenClienteIsDeleted(){
+        Long clienteID=1L;
+        willDoNothing().given(clienteRepository).delete(any());
+        clienteService.deleteByID(clienteID);
+        verify(clienteRepository, times(1)).delete(any());
+    }
+}
